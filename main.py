8105ff856a5711b0ee9d7873a31a8a85896bffa1
Ana Luisa Espinoza López <ana.espinoza@iteso.mx>
2025-10-06 18:05:35 -0600

    new plots and calc of returns in main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from optimize import run_optuna
 import pandas as pd
-from utils import split_data, plot_portfolio_subplots, plot_portfolio_history, evaluate_best_params, plot_all_metrics_heatmap, plot_metrics_summary_table 
+from utils import split_data, plot_portfolio_subplots, plot_portfolio_history, evaluate_best_params, calculate_returns_summary, plot_metrics_summary_table, plot_returns_summary_table
 
 
 
@@ -19,16 +19,31 @@ def main():
         print(f"{k}: {v}")
 
     # 3️⃣ Evaluar en train, test y val
-    portfolios_dict, metrics_dict = evaluate_best_params(train_df, test_df, val_df, study.best_params)
+    full_df = pd.concat([train_df, test_df, val_df])
+    portfolios_dict, metrics_dict = evaluate_best_params(train_df, test_df, val_df,full_df, study.best_params)
 
-    # 4️⃣ Graficar resultados
-    fig1 = plot_portfolio_subplots(portfolios_dict, metrics_dict)
+    
+    train_hist = portfolios_dict['train']
+    test_hist = portfolios_dict['test']
+    val_hist = portfolios_dict['val']
+    full_hist = portfolios_dict['full']
+
+    summary_df = calculate_returns_summary(train_hist,test_hist,val_hist, full_hist)
+    fig1 = plot_returns_summary_table(summary_df, title="Annualized Returns by Period")
     fig1.show()
+    
+    
+    # 4️⃣ Graficar resultados
+    fig2 = plot_portfolio_subplots(portfolios_dict, metrics_dict)
+    fig2.show()
+
+    fig3 = plot_portfolio_history(portfolios_dict)
+    fig3.show()
 
     f = ['Monthly', 'Quarterly', 'Yearly']
     for i in f:
-        fig2 = plot_metrics_summary_table(metrics_dict, freq=i)
-        fig2.show()
+        fig4 = plot_metrics_summary_table(metrics_dict, freq=i)
+        fig4.show()
 
 if __name__ == "__main__":
     main()
